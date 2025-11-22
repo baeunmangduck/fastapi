7a72776303f97278936108bd16e58b1c521aea74
baeunmangduck <rladmsqo950907@gmail.com>
2025-11-22 10:51:56 +0900

    path paramter & query parameter

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
+@app.get("/", summary="루트", tags=["test1"])
+async def root():
+    
+    '''
+    이것은 간단한 API입니다. 
+    '''
     return {"msg": "Hello FastAPI!"}
+
+@app.get("/items/{id}", tags=["test2"])
+async def read(id: int):
+    return {"id": id}
+
+
+@app.get("/users/{name}/age/{age}")
+async def read_user_name_age(name, age):
+    return {"name": name, "age": age}
+
+@app.get("/users/")
+async def read_user(name="mike", age=32):
+    return {"name": name, "age": age}
