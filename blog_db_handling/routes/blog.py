f03bce02f2ab97faff89b241ad8ff7094fa73621
baeunmangduck <rladmsqo950907@gmail.com>
2025-12-31 19:57:34 +0900

    add context_get_conn & bind params

--- blog_db_handling/routes/blog.py
+++ blog_db_handling/routes/blog.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request
-from sqlalchemy import text
-from db.database import direct_get_conn
+from fastapi import APIRouter, Depends, Request, status
+from fastapi.exceptions import HTTPException
+from sqlalchemy import Connection, text
+from db.database import context_get_conn, direct_get_conn
 from schemas.blog_schema import Blog, BlogData
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -36,3 +37,39 @@ async def get_all_blogs(request: Request):
     finally:
         if conn:
             conn.close()
+
+
+@router.get("/show/{id}")
+def get_blog_by_id(
+    request: Request, id: int, conn: Connection = Depends(context_get_conn)
+):
+    try:
+        q = """
+            SELECT id, title, author, content, image_loc, modified_dt FROM blog 
+            where id = :id
+            """
+
+        statement = text(q)
+
+        result = conn.execute(statement, {"id": id})
+        if result.rowcount == 0:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"해당 ID {id}는(은) 존재하지 않음",
+            )
+
+        row = result.fetchone()
+        blog = BlogData(
+            id=row.id,
+            title=row.title,
+            author=row.author,
+            content=row.content,
+            image_loc=row.image_loc,
+            modified_dt=row.modified_dt,
+        )
+
+        result.close()
+        return blog
+    except SQLAlchemyError as e:
+        print(e)
+        raise e
