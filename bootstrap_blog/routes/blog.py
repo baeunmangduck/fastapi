f07d30b1d9dbbc13cfc1db67a61be277d4605aae
baeunmangduck <rladmsqo950907@gmail.com>
2026-01-17 13:33:50 +0900

    image_loc 관련 예외처리 추가

--- bootstrap_blog/routes/blog.py
+++ bootstrap_blog/routes/blog.py
@@ -49,10 +49,16 @@ def create_blog(
     imagefile: UploadFile | None = File(None),
     conn: Connection = Depends(context_get_conn),
 ):
-    print("### image File", imagefile)
-    print("Image file name", imagefile.filename)
-    blog_svc.upload_file(author=author, imagefile=imagefile)
-    # blog_svc.create_blog(conn, title=title, author=author, content=content, image)
+    image_loc = None
+    if len(imagefile.filename.strip()) > 0:
+        image_loc = blog_svc.upload_file(author=author, imagefile=imagefile)
+        blog_svc.create_blog(
+            conn, title=title, author=author, content=content, image_loc=image_loc
+        )
+    else:
+        blog_svc.create_blog(
+            conn, title=title, author=author, content=content, image_loc=image_loc
+        )
     return RedirectResponse("/blogs", status_code=status.HTTP_302_FOUND)
 
 
