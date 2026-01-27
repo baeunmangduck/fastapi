4cb820d01685fb255e81b9ceeed7722d57eeab25
baeunmangduck <rladmsqo950907@gmail.com>
2026-01-27 20:45:07 +0900

    add lifespan

--- bootstrap_blog/main.py
+++ bootstrap_blog/main.py
@@ -1,7 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes import blog
+from db.database import engine
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("starting up....")
+    yield
+
+    print("shutting down...")
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(blog.router)
