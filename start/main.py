935cd7429fd0c4d7d4477d65855dbf1f751eb68a
baeunmangduck <rladmsqo950907@gmail.com>
2026-02-25 21:10:20 +0900

    modify table and blog_svc module

--- start/main.py
+++ start/main.py
@@ -1,11 +1,16 @@
+import os
+
 from fastapi import FastAPI
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 from routes import blog, auth
 from utils.common import lifespan
 from utils import exc_handler, middleware
+from dotenv import load_dotenv
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -19,7 +24,11 @@ app.add_middleware(
     allow_credentials=True,
     max_age=-1,
 )
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 # app.add_middleware(middleware.DummyMiddleware)
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY, max_age=3600)
 app.add_middleware(middleware.MethodOverrideMiddlware)
 
 app.include_router(blog.router)
